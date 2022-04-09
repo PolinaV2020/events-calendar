0aed27c2bed09b4065133396b242c0219d3df5d9
polinav2020 <polinapiksi2019@mail.ru>
2022-04-09 13:32:50 +0300

    fix: routes

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import {privateRoutes, publicRoutes} from '../router/index';
-
+import {privateRoutes, publicRoutes} from '../router/routes';
 
 export const AppRouter = () => {
     const auth = true;
     return (
-        auth ?
             <Routes>
-                {privateRoutes.map(route =>
-                    <Route path={route.path} element={route.element} />
-                )}
-            </Routes>
-            :
-            <Routes>
-                {publicRoutes.map(route =>
-                    <Route path={route.path} element={route.element} />
-                )}
+                {auth ?
+                privateRoutes.map(route =>
+                    <Route path={route.path} element={route.element} key={route.path}/>
+                )
+                :
+                publicRoutes.map(route =>
+                    <Route path={route.path} element={route.element} key={route.path}/>
+                )
+                }
             </Routes>
     );
 };
